62395767dc1301d9b69593fc4aac82a269297033
Adrian Castravete <adrian@figshare.com>
2020-02-24 11:17:40 +0200

    Use coroutines to be able to show load progress

--- level.lua
+++ level.lua
@@ -26,17 +26,22 @@ function Level:init(width, height)
 
   self._levelWidth = w
   self._levelHeight = h
-  self.cameraX = 1024
+  self.cameraX = w * 16
   self.cameraY = 0
   self._img = BB.makeTileset("blocks.png")
-  self._map = self:generateMap(w, h)
+  self._state = 'initial'
+  self._map = nil
+  self._co = nil
 end
 
 function Level:generateMap(width, height)
   local m = {}
   local brd = 5
 
+  self._state = 'creating'
   log.info("Starting level generation for a ("..width..", "..height..")")
+
+  coroutine.yield('generation', 0)
 
   log.debug("Firstly setting initial random values for the map")
   for j=1, height do
@@ -81,11 +86,30 @@ function Level:generateMap(width, height)
         k = v,
       }
     end
+    coroutine.yield('generation', j / height)
   end
   local v = rnd(255)
   while v == self._lastV do v = rnd(255) end
   self._lastV = v
   m[6][width-8].k = 'dirt'
+  coroutine.yield('generation', 1)
+
+  log.debug("Generating grass...")
+  local y = 1
+  for i=2, width-8 do
+    for j=1, y-1 do
+      m[j][i].k = nil
+    end
+    m[y][i].k = 'grass'
+    if rnd() < 0.5 then
+      if rnd() < 1 / (1+y*1.2) then
+        y = y + 1
+      elseif y > 1 then
+        y = y - 1
+      end
+    end
+  end
+  coroutine.yield('grass', 1)
 
   log.debug("Initiating floodfill for path to every block")
   local accum = {{width-8, 6}}
@@ -103,23 +127,11 @@ function Level:generateMap(width, height)
     if i < width then vv(i+1, j) end
     if j < height then vv(i, j+1) end
     index = index + 1
-  end
-
-  log.debug("Generating grass...")
-  local y = 1
-  for i=2, width-8 do
-    for j=1, y-1 do
-      m[j][i].k = nil
-    end
-    m[y][i].k = 'grass'
-    if rnd() < 0.5 then
-      if rnd() < 1 / (1+y*1.2) then
-        y = y + 1
-      elseif y > 1 then
-        y = y - 1
-      end
+    if index % 1000 == 1 then
+      coroutine.yield('floodfill', index / (width * height))
     end
   end
+  coroutine.yield('floodfill', 1)
 
   log.debug("Setting 'bedRock' for all untouched blocks")
   for j=1, height do
@@ -130,7 +142,9 @@ function Level:generateMap(width, height)
         m[j][i].k = 'bedRock'
       end
     end
+    coroutine.yield('bedrock', j / height)
   end
+  coroutine.yield('bedrock', 1)
 
   log.debug("Drawing tiles")
   for j=1, height do
@@ -144,22 +158,45 @@ function Level:generateMap(width, height)
         BB.tileClear(x, y, 2, 2, 0, 0, 1, {0, 0.25, 1})
       end
     end
+    coroutine.yield('drawing', j / height)
   end
+  coroutine.yield('drawing', 1)
+
+  self.cameraX = width * 16
+  self.cameraY = 0
 
   log.info("Level generation completed.")
 
-  return m
+  self._state = 'playing'
+
+  return 'done', m
 end
 
 function Level:update(dt)
   local w = self._levelWidth
   local h = self._levelHeight
-  local cx, cy = self.cameraX, self.cameraY
-  cx = math.min(w * 16 - 119, math.max(0, cx))
-  cy = math.min(h * 16 - 119, math.max(0, cy))
-  self.cameraX, self.cameraY = cx, cy
+  if self._state == 'initial' then
+    self._co = coroutine.create(self.generateMap)
+    local err, other = coroutine.resume(self._co, self, w, h)
+  elseif self._state == 'creating' then
+    local err, st, prog = coroutine.resume(self._co, self)
+    if not err then
+      self:setError(st)
+    else
+      if st == 'done' then
+        self._map = prog
+      else
+        BB.printXY(0, 0, st..": "..tostring(math.floor(prog*100)).."%")
+      end
+    end
+  elseif self._state == 'playing' then
+    local cx, cy = self.cameraX, self.cameraY
+    cx = math.min(w * 16 - 119, math.max(0, cx))
+    cy = math.min(h * 16 - 119, math.max(0, cy))
+    self.cameraX, self.cameraY = cx, cy
 
-  BB.draw(0, 0, w*2, h*2, -cx, -cy)
+    BB.draw(0, 0, w*2, h*2, -cx, -cy)
+  end
 end
 
 return Level
